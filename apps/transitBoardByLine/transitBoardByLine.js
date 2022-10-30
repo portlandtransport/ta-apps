850044bbc68e73372ed0badd8ef060a0d213000e
Chris Smith <chris.smith@xerox.com>
2022-10-29 18:50:42 -0700

    more testing

--- apps/transitBoardByLine/transitBoardByLine.js
+++ apps/transitBoardByLine/transitBoardByLine.js
@@ -243,7 +243,7 @@ transitBoardByLine.initializePage = function(data) {
 				  free_bikes = data.optionsConfig.include_free_bikes[0];
 				}
 				if (transitBoardByLine.gbfs != 0 ) {
-					console.log(typeoff trGBFS);
+					console.log(typeof trGBFS);
 					transitBoardByLine.bikes = new trGBFS({
 						lat: data.optionsConfig.lat[0],
 						lng: data.optionsConfig.lng[0],
