69fc79b4e97ad023fab34c31963bdb34050657f3
Chris Smith <chris.smith@xerox.com>
2024-03-05 15:24:05 -0800

    remove explicit post

--- apps/assets/js/trGBFS.js
+++ apps/assets/js/trGBFS.js
@@ -321,7 +321,6 @@ function trGBFS(options) {
 	function initialize_feeds() {
 		jQuery.ajax({
 			// url: gbfs_obj.gbfs_feed_proxy_prefix+gbfs_obj.gbfs_feed,
-			type: 'POST',
 			url: gbfs_obj.gbfs_feed,
 			/*headers: {
 				'x-cors-api-key': gbfs_obj.gbfs_feed_proxy_key
