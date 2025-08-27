66ab8236a7e503f9e1c5c9f8d6d963abdebfd7ec
Chris Smith <chris.smith@xerox.com>
2025-08-26 21:00:36 -0700

    passio 28

--- apps/assets/js/trArrServicePassio.js
+++ apps/assets/js/trArrServicePassio.js
@@ -105,9 +105,12 @@ function trArrPassioUpdater(service_requests,arrivals_object,avl_agency_id,agenc
 					var arrival_trips = trips[stop.stop_id];
 					var stop_trips = stop.stop_data.trips;
 					// match trips
+					console.log(stop_trips);
+					/*
 					stop_trips.keys.forEach((trip_id) => {
 						console.log(trip_id);
 					})
+						*/
 				}
 
 				
