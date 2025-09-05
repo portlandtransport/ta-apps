5c7c86c8c347d6f690dce5f5d6d57589f55d2a8b
Chris Smith <chris.smith@xerox.com>
2025-09-05 11:48:33 -0700

    another try at native health check

--- apps/assets/js/trArr.js
+++ apps/assets/js/trArr.js
@@ -645,7 +645,7 @@ function trArr(input_params) {
 							if (is_development() && trArrSupportsCors()) {
 
 								var data = { timestamp: arrivals_object.start_time, start_time: arrivals_object.start_time, version: arrivals_object.version, id: arrivals_object.id, application_id: arrivals_object.input_params.applicationId, application_name: arrivals_object.input_params.applicationName, application_version: arrivals_object.input_params.applicationVersion, application_host: window.location.protocol+'//'+window.location.host+'/', "height": jQuery(window).height(), "width": jQuery(window).width(), "platform": platform };
-								//this.health_update(data,0);
+								arrivals_object.health_update(data,0);
 
 							}
 							
