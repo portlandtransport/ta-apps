83c8eaaed29973138f5070054b38e89a8387eadc
Chris Smith <chris.smith@xerox.com>
2025-09-09 12:38:26 -0700

    roll out new health update for all but cyan

--- apps/assets/js/trArr.js
+++ apps/assets/js/trArr.js
@@ -687,9 +687,9 @@ function trArr(input_params) {
 
 							
 
-							if (is_development() && trArrSupportsCors() && !is_cyan()) {
+							if (trArrSupportsCors() && !is_cyan()) {
 
-								console.log('new health check');
+								//console.log('new health check');
 
 								var data = { timestamp: arrivals_object.start_time, start_time: arrivals_object.start_time, version: arrivals_object.version, id: arrivals_object.id, application_id: arrivals_object.input_params.applicationId, application_name: arrivals_object.input_params.applicationName, application_version: arrivals_object.input_params.applicationVersion, application_host: window.location.protocol+'//'+window.location.host+'/', "height": jQuery(window).height(), "width": jQuery(window).width(), "platform": platform };
 								arrivals_object.health_update(arrivals_object,data,0);
@@ -702,7 +702,7 @@ function trArr(input_params) {
 
 							} else {
 
-								console.log('legacy health check');
+								//console.log('legacy health check');
 								
 								jQuery.ajax({
 										dataType: access_method,
