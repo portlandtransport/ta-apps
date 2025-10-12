feb3aac1666476eba8fbae9425b7cc95b64e3ea2
Chris Smith <chris.smith@xerox.com>
2025-10-12 16:52:58 -0700

    option, not options

--- apps/assets/js/trLoader.js
+++ apps/assets/js/trLoader.js
@@ -355,8 +355,8 @@ function trLoader(hwid,url_fix) {
 			console.log(url_fix);
 			var url = jQuery.jurlp(theurl.app_url);
 			url.query({"appl[id]":url_fix.id});
-			url.query({"options[nickname]":url_fix.nickname});
-			url.query({"options[parentname]":url_fix.parentname});
+			url.query({"option[nickname]":url_fix.nickname});
+			url.query({"option[parentname]":url_fix.parentname});
 			console.log(url);
 			console.log(url.href);
 		} else {
