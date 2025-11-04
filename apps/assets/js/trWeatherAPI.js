1355e62574e50713a91f32624b530ae5fd3a428b
Chris Smith <chris.smith@xerox.com>
2025-11-04 07:52:39 -0800

    use xhr for weather

--- apps/assets/js/trWeatherAPI.js
+++ apps/assets/js/trWeatherAPI.js
@@ -42,7 +42,7 @@
 				}			
 
 				
-				this.update_forecast = function() {
+				this.update_forecast_old = function() {
 				    
 				    var api_key = trWeatherCredentials('WeatherAPI');
                     var weather_url = "https://api.weatherapi.com/v1/current.json?key="+api_key+"&q="+options.lat+","+options.lng+"&aqi=yes";
@@ -70,6 +70,64 @@
 						}
 
 					});
+				}
+
+				this.update_forecast = function(retry_count) {
+					const retry_limit = 1;
+
+					var api_key = trWeatherCredentials('WeatherAPI');
+                    var weather_url = "https://api.weatherapi.com/v1/current.json?key="+api_key+"&q="+options.lat+","+options.lng+"&aqi=no";
+
+					const xhr = new XMLHttpRequest();
+
+					xhr.responseType = 'text';
+
+					xhr.open('GET', weather_url, true);
+
+					// Set up the event handler for when the request state changes
+					xhr.onreadystatechange = function() {
+						// Check if the request is complete (readyState 4) and successful (status 200)
+
+						if (xhr.readyState === 4 && xhr.status === 200) {
+							console.log("Retry: "+retry_count);
+							//console.log("Text response: "+xhr.responseText);
+
+							// now try parsing json
+							try {
+								var data = JSON.parse(xhr.responseText);
+								// Process data
+								console.log(response_data);
+								if ( typeof response_data != "undefined") {
+									// process here
+									if (data.current && data.current.condition) {
+										weather.summary = data.current.condition.text;
+										weather.temperature = Math.floor(data.current.temp_f + 0.5)+"&deg;";
+										var icon_url = data.current.condition.icon;
+										weather.icon = '<img src="'+icon_url+'" style="height: 1.4em; margin: -0.2em">';
+										weather.timestamp = new Date();
+									}
+								}
+							} catch (e) {
+								//console.log("json parsing error");
+								//console.log(e);
+								if (retry_count >= retry_limit) {
+									if (typeof newrelic === "object") {
+										newrelic.addPageAction("WE9: Weather JSON parsing error");
+									}
+								}
+							}
+
+						} else if (xhr.readyState === 4 && xhr.status !== 200) {
+							//console.log("xhr state error");
+							if (retry_count >= retry_limit) {
+								if (typeof newrelic === "object") {
+									newrelic.addPageAction("WE"+xhr.status+": Weather response error "+xhr.statusText,{'errorText': xhr.statusText, 'errorThrown': xhr.status});
+								}
+							} else {
+								weather.update_forecase(retry_count+1);
+							}
+						}
+					};
 				}
 				
 				weather.update_forecast();				
