70dcfabf7ee9f49f9f2119d5e53ed144e73adf47
Chris Smith <chris.smith@xerox.com>
2026-07-13 11:53:31 -0700

    more logging

--- apps/assets/js/trArrServicePassio.js
+++ apps/assets/js/trArrServicePassio.js
@@ -122,6 +122,9 @@ function trArrPassioUpdater(service_requests,arrivals_object,avl_agency_id,agenc
 					var arrival_trips = trips[stop.stop_id];
 					var stop_trips = stop.stop_data.trips;
 					// match trips
+
+					console.log("Stop trips:");
+					console.log(stop_trips);
 					
 					var targeted_trip_routes = {};
 					Object.keys(arrival_trips).forEach((trip_id) => {
