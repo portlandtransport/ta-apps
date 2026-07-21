d959e1a06be89509fcb387445428f0ae7f88e3eb
Chris Smith <chris.smith@xerox.com>
2026-07-21 10:11:29 -0700

    log stop config

--- apps/assets/js/trStopCache.js
+++ apps/assets/js/trStopCache.js
@@ -41,6 +41,8 @@ function trStopCache() {
 	
   	
 	this.checkCached = function(arrivals_object,stop_config,callback) {
+		console.log("Stop config:");
+		console.log(stop_config);
 		// initialize recursive retrieval of cache items
 		var next_stop = this.nextUncachedStop(stop_config);
 		this.getCacheItem(arrivals_object,stop_config,next_stop,callback);
