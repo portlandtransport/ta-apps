ad3736db2bbe39895651f0a861b9a9a1e6ce0e47
Chris Smith <chris.smith@xerox.com>
2026-07-25 14:13:35 -0700

    log sorter issue to New Relic

--- apps/assets/js/libraries/jquery.isotope.js
+++ apps/assets/js/libraries/jquery.isotope.js
@@ -580,6 +580,9 @@
     _getSorter : function( elem, sortBy ) {
       console.log('_getSorter');
       if (typeof($.data( elem, 'isotope-sort-data' )) == "undefined") {
+        if (typeof newrelic === "object") {
+          newrelic.addPageAction("ISO99 Sorter error");
+        }
         return 0;
       }
       return $.data( elem, 'isotope-sort-data' )[ sortBy ];
