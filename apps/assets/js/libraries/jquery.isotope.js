053f638ae765898f58ef5460349ac6fd929f69f4
Chris Smith <chris.smith@xerox.com>
2026-07-27 14:01:51 -0700

    log outerhtml

--- apps/assets/js/libraries/jquery.isotope.js
+++ apps/assets/js/libraries/jquery.isotope.js
@@ -581,6 +581,7 @@
 
     _getSorter : function( elem, sortBy ) {
       //console.log('_getSorter');
+      console.log(elem.outerHTML);
       if (typeof($.data( elem, 'isotope-sort-data' )) == "undefined") {
         if (typeof newrelic === "object") {
           newrelic.addPageAction("ISO99 Sorter error "+$.data( elem, 'tripid' ));
