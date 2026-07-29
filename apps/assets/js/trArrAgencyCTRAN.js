21c955e3b44cd451bdca3ccf5c7c5f2d104e95d7
Chris Smith <chris.smith@xerox.com>
2026-07-29 09:31:14 -0700

    start working on ctran colors

--- apps/assets/js/trArrAgencyCTRAN.js
+++ apps/assets/js/trArrAgencyCTRAN.js
@@ -51,7 +51,7 @@ function trArrAgencyCTRANMakeServiceRequests(stops,options,stop_cache,service_re
 }
 
 function trArrAgencyCTRANEntryFilterCallback(entry) {
-	//console.log(entry);
+	console.log(entry);
 	entry.route_data.route_short_name = entry.route_data.route_id;
 	if (entry.route_data.route_id == "106") {
 		entry.route_data.route_short_name = "105X";
