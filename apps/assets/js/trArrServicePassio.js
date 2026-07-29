308f234669ae3c5db5a25ed5b4f964cef3d519e9
Chris Smith <chris.smith@xerox.com>
2026-07-29 14:36:03 -0700

    log stop trips

--- apps/assets/js/trArrServicePassio.js
+++ apps/assets/js/trArrServicePassio.js
@@ -112,8 +112,8 @@ function trArrPassioUpdater(service_requests,arrivals_object,avl_agency_id,agenc
 		
 		updater.process_results = function(trips) {
 
-			console.log("Trips:");
-			console.log(trips);
+			//console.log("Trips:");
+			//console.log(trips);
 			updater.update_connection_health(true);
 			var local_queue = [];
 			var update_time = localTime().getTime();
@@ -126,8 +126,8 @@ function trArrPassioUpdater(service_requests,arrivals_object,avl_agency_id,agenc
 					var stop_trips = stop.stop_data.trips;
 					// match trips
 
-					//console.log("Stop trips:");
-					//console.log(stop_trips);
+					console.log("Stop trips:");
+					console.log(stop_trips);
 					
 					var targeted_trip_routes = {};
 					Object.keys(arrival_trips).forEach((trip_id) => {
