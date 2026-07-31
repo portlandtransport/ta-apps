c624c2efb7f76b72cfed42b14b5c9b2ae021e87e
Chris Smith <chris.smith@xerox.com>
2026-07-31 16:35:26 -0700

    reduce logging

--- apps/assets/js/trArrServicePassio.js
+++ apps/assets/js/trArrServicePassio.js
@@ -178,13 +178,14 @@ function trArrPassioUpdater(service_requests,arrivals_object,avl_agency_id,agenc
 										*/
 										if (typeof stop.callback == 'function') {
 											local_queue.push(stop.callback(entry));
-											
+											/*
 											if (entry.route_id == "050") {
 												console.log(entry);
 												console.log("callback pushing "+entry.trip_id);
 												console.log("seen");
 												console.log(trips_seen);
 											}
+											*/
 										} else {
 											local_queue.push(entry);
 										}
