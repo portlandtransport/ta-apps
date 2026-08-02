5a4ebe458bbc85e6cc1680cec517ed861eab3871
Chris Smith <chris.smith@xerox.com>
2026-08-02 09:31:01 -0700

    2nd try on headsign

--- apps/assets/js/trArrAgencyCTRAN.js
+++ apps/assets/js/trArrAgencyCTRAN.js
@@ -70,7 +70,7 @@ function trArrAgencyCTRANEntryFilterCallback(entry) {
 	}
 	entry.route_data.route_short_name = entry.route_data.route_short_name.replace(/^0+/, ''); 
 
-	entry.headsign = entry.headsign = entry.headsign+" to "+entry.trip_data.trip_headsign;
+	entry.headsign = entry.headsign = entry.headsign+" to "+entry.route_data.trip_data.trip_headsign;
 
 	if (entry.stop_data.stop_lat < 45.5852698) {
 		entry.headsign = "C-TRAN "+entry.headsign;
