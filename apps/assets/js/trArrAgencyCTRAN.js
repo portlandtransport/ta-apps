ac81a276c1ff7c55a2ce82b2895909083ac8a0df
Chris Smith <chris.smith@xerox.com>
2026-08-02 09:35:31 -0700

    logging

--- apps/assets/js/trArrAgencyCTRAN.js
+++ apps/assets/js/trArrAgencyCTRAN.js
@@ -52,13 +52,7 @@ function trArrAgencyCTRANMakeServiceRequests(stops,options,stop_cache,service_re
 
 function trArrAgencyCTRANEntryFilterCallback(entry) {
 	// PDX lat 45.5852698
-	//console.log("raw");
-	/*
-	if (false && entry.route_id == "051") {
-		console.log(entry.route_id+" "+entry.stop_id+" "+entry.trip_id);
-		console.log(entry);
-	}
-	*/
+	console.log(entry);
 
 	entry.route_data.route_short_name = entry.route_data.route_id;
 	if (entry.route_data.route_id == "106") {
@@ -70,7 +64,7 @@ function trArrAgencyCTRANEntryFilterCallback(entry) {
 	}
 	entry.route_data.route_short_name = entry.route_data.route_short_name.replace(/^0+/, ''); 
 
-	entry.headsign = entry.headsign = entry.headsign+" to "+entry.route_data.trip_data.trip_headsign;
+	//entry.headsign = entry.headsign = entry.headsign+" to "+entry.route_data.trip_data.trip_headsign;
 
 	if (entry.stop_data.stop_lat < 45.5852698) {
 		entry.headsign = "C-TRAN "+entry.headsign;
