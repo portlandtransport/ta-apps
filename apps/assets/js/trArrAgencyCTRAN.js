e096f9d6d2a93b9cebcc9d68c93c4f48808b3d84
Chris Smith <chris.smith@xerox.com>
2026-08-03 06:51:23 -0700

    headsigns

--- apps/assets/js/trArrAgencyCTRAN.js
+++ apps/assets/js/trArrAgencyCTRAN.js
@@ -65,7 +65,7 @@ function trArrAgencyCTRANEntryFilterCallback(entry) {
 	entry.route_data.route_short_name = entry.route_data.route_short_name.replace(/^0+/, ''); 
 
 	if (Object.hasOwn(entry.stop_data,'trip_data')) {
-		entry.headsign = entry.headsign = entry.headsign+" to "+entry.stop_data.trip_data.trip_headsign;
+		entry.headsign = entry.headsign = entry.headsign+" to "+entry.stop_data.trip_data[entry.trip_id].trip_headsign;
 	}	
 
 	if (entry.stop_data.stop_lat < 45.5852698) {
