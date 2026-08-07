c5136bde45fefd92843654df337b216acc215efe
Chris Smith <chris.smith@xerox.com>
2026-08-06 17:24:11 -0700

    another deploy attempt

--- apps/transitBoardByLine/transitBoardByLine.js
+++ apps/transitBoardByLine/transitBoardByLine.js
@@ -1007,7 +1007,7 @@ transitBoardByLine.displayPage = function(data, callback) {
 			var id = removal_queue.shift();
 			//console.log("try to remove "+id);
 			transitBoardByLine.isotope_container.isotope( 'remove', jQuery("table."+id) );
-			// remove from DOM
+			// remove from DOM 
 			jQuery("table."+id).remove();
 			process_removals();
 		} else {
